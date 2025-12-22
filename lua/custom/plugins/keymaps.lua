1bdce66edb251b9d2371c1c5c515b9fb2aaed42a
Philipp Aaron Becker <p.becker@green-fusion.de>
2025-12-22 12:10:09 +0100

    use :Ex and :Rex with <leader>e

--- lua/custom/plugins/keymaps.lua
+++ lua/custom/plugins/keymaps.lua
@@ -1,6 +1,13 @@
 # personal keymaps
 
-vim.keymap.set("n", "<leader>e", ":Ex<CR>", { desc = "Open netrw" })
+-- vim.keymap.set("n", "<leader>e", ":Ex<CR>", { desc = "Open netrw" })
+
+vim.keymap.set("n", "<leader>e", function()
+  local ok = pcall(vim.cmd, "Rexplore")
+  if not ok then
+    vim.cmd("Ex")
+  end
+end, { desc = "Return to netrw or open explorer" })
 
 print("Hello vim")
 
