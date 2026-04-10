b3f51fb1170c1be417546312811e24e489e7f746
Philipp Aaron Becker <philipp@local>
2026-04-10 17:57:37 +0200

    fix nvim-tressitter setup

--- init.lua
+++ init.lua
@@ -957,8 +957,10 @@ require('lazy').setup({
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
-    main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
+    config = function(_, opts)
+      require('nvim-treesitter').setup(opts)
+    end,
     opts = {
       ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'python', 'hurl' },
       -- Autoinstall languages that are not installed
