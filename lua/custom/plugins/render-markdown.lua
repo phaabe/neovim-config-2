cdec1222c76c49d373ca98c35193f851eedb1c4b
phaabe <philipp@local>
2026-05-22 15:57:40 +0200

    update render-markdown

--- lua/custom/plugins/render-markdown.lua
+++ lua/custom/plugins/render-markdown.lua
@@ -1,5 +1,24 @@
 return {
   'MeanderingProgrammer/render-markdown.nvim',
   dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' },
-  opts = {},
+  opts = {
+    heading = {
+      icons = { '# ', '## ', '### ', '#### ', '##### ', '###### ' },
+    },
+  },
+  -- opts = {
+  --   heading = {
+  --     icons = { '# ', '## ', '### ', '#### ', '##### ', '###### ' },
+  --   },
+  --   code = {
+  --     sign = false,
+  --   },
+  --   bullet = {
+  --     icons = { '•', '◦', '▪', '▫' },
+  --   },
+  --   checkbox = {
+  --     unchecked = { icon = '[ ]' },
+  --     checked = { icon = '[x]' },
+  --   },
+  -- },
 }
